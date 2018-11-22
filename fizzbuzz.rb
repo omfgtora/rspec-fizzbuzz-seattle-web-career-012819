530245a73ff7e3276db6d4c45ca75249c586a4cd
reflective-mapping-9478 <ejrobe@gmail.com>
2018-11-22 04:10:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-  if (x % 3 = 0)
+  if (x % 3 == 0)
     return "fizz"
-  elsif (x % 5 = 0)
+  elsif (x % 5 == 0)
     return "buzz"
-  elsif ((x % 3 = 0) && (x % 5 = 0))
+  elsif ((x % 3 == 0) && (x % 5 == 0))
     return "fizzbuzz"
   else
     return NIL
